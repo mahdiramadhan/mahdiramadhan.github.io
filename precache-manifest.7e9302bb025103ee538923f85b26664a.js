68261579543bad1f1fc0f785cd2d07a86fe442ec
mahdiramadhan <ramdhanslif@gmail.com>
2022-06-16 20:44:41 +0700

    Updates

---
+++ precache-manifest.7e9302bb025103ee538923f85b26664a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "310772ed50730250bb48",
+    "url": "/static/css/main.441e4b81.chunk.css"
+  },
+  {
+    "revision": "310772ed50730250bb48",
+    "url": "/static/js/main.310772ed.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "92cefb059b777566960a",
+    "url": "/static/css/2.ddd6223a.chunk.css"
+  },
+  {
+    "revision": "92cefb059b777566960a",
+    "url": "/static/js/2.92cefb05.chunk.js"
+  },
+  {
+    "revision": "82555cf690c0363f7ef9",
+    "url": "/static/js/runtime~main.82555cf6.js"
+  },
+  {
+    "revision": "720529fd21d0ecbaacebb88e183212d4",
+    "url": "/index.html"
+  }
+];
